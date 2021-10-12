1e2b3cc98d1cabaf1bd282865fe6677a263a8a94
krendeleno <krendeleno@gmail.com>
2021-10-13 01:35:36 +0300

    small fixes

---
+++ src/colors.ts
@@ -0,0 +1,38 @@
+import {backgroundColors, effects, fontColors, Reset} from './model';
+
+function addColor(text: string, color: string, isBackground = false) {
+    if (isBackground) {
+        return text + backgroundColors[color];
+    }
+    return text + fontColors[color];
+}
+
+function getEffects(effectList: string[]) {
+    return effectList.map(effect => effects[effect]).join('');
+}
+
+type OptionsColor = {
+    font?: string;
+    background?: string;
+    effects?: string[];
+}
+
+export function color(text: string, options: OptionsColor) {
+    const preparedText = text.replace(/ё/g, 'е');
+    let result = '';
+    if (options) {
+        if (options.font) {
+            result = addColor(result, options.font);
+        }
+        if (options.background) {
+            result = addColor(result, options.background, true);
+        }
+        if (options.effects) {
+            result += getEffects(options.effects);
+        }
+        result += preparedText;
+        result += Reset;
+        return result;
+    }
+    return preparedText;
+}
---
+++ src/style.ts
@@ -0,0 +1,20 @@
+import { color } from './colors';
+import { markdown } from './md';
+
+
+type OptionsColor = Parameters<typeof color>[1];
+type OptionsMd = Parameters<typeof markdown>[1];
+type Options = OptionsMd | OptionsColor;
+
+export function style(text: string, options: Options) {
+    if (text.length === 0) {
+        return text;
+    }
+    if ('font' in options || 'background' in options || 'effects' in options) {
+        return color(text, options);
+    }
+    if ('bold' in options || 'italic' in options || 'mono' in options || 'link' in options) {
+        return markdown(text, options);
+    }
+    return text;
+}
